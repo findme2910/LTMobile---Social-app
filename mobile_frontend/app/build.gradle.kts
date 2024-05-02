f1f6b366c875bedf89bae03306e35978e12c730a
findme2910 <21130514@st.hcmuaf.edu.vn>
2024-05-02 17:09:59 +0700

    layout Notification

--- mobile_frontend/app/build.gradle.kts
+++ mobile_frontend/app/build.gradle.kts
@@ -29,6 +29,8 @@ android {
 }
 
 dependencies {
+
+    implementation ("androidx.recyclerview:recyclerview:1.3.0")
     implementation ("org.projectlombok:lombok:1.18.22")
     annotationProcessor ("org.projectlombok:lombok:1.18.22")
     compileOnly ("org.projectlombok:lombok:1.18.22")
@@ -41,4 +43,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
