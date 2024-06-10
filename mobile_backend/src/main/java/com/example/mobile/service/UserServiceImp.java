8cd96976dc60ab286e4b6d6affc8d56f34fd407e
QuanghuyDrawn <21130376@st.hcmuaf.edu.vn>
2024-06-10 09:03:42 +0700

    update

--- mobile_backend/src/main/java/com/example/mobile/service/UserServiceImp.java
+++ mobile_backend/src/main/java/com/example/mobile/service/UserServiceImp.java
@@ -292,6 +292,7 @@ public class UserServiceImp implements UserService {
 		addTo.getReplys().add(comment);
 		commentRepository.save(comment);
 		commentRepository.save(addTo);
+		notificationService.replyNoti(addTo.getPost());
 	}
 
 	@Override
